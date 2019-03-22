75df537854c3471a3f4a2864d3606fa23bd3d97a
heisstobias <sylux.tobi@hotmail.com>
2019-03-22 20:01:13 +0100

    Schnelles erstellen der Klasse City und Anfang der Klasse HammurabiGame

--- game/City.py
+++ game/City.py
@@ -1,3 +1,49 @@
 class City:
 
     population =  0
+    acres = 0
+    tradeValue = 0
+    bushelsPerAcre = 0
+    store = 0
+    year = 0
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def process(self):
+        """
+            Wird aufgerufen wenn die Eingabe bestätigt wird
+        :return:
+        """
+
+    def calculatePopulation(self, value):
+        """
+            Wird aufgerufen, wenn die Population verändert wird
+        :return:
+        """
+
+    def calculateStore(self, value):
+        """
+            Wird aufgerufen, wenn die Scheffelmenge verändert wird
+        :return:
+        """
+
+    def disease(self):
+        """
+            Wird aufgerufen in der process() und berechnet Wahrscheinlichkeit einer
+            Plage oder Seuche
+        :return:
+        """
+
+    def rats(self):
+        """
+            Wird in der process() aufgerufen und berechnet die Menge an Scheffeln,
+            welche die Ratten pro Jahr fressen
+        :return:
+        """
+
+    def immigration(self):
+        """
+            Wird aufgerufen in der process() und berechnet den Bevölkerungszuwachs
+        :return:
+        """
---
+++ game/HammurabiGame.py
@@ -0,0 +1,7 @@
+class HammurabiGame:
+
+    def main(self):
+        """
+            Hauptmethode, welche beim Spielstart ausgeführt wird und bis Spielende durchläuft
+
+        """
