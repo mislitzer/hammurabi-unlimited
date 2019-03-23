8498830777b0d274be62be9e311c80a052cb00c2
Martin <im@prepress-digital.com>
2019-03-23 12:07:16 +0100

    fixed start issues

--- game/HammurabiGame.py
+++ game/HammurabiGame.py
@@ -2,16 +2,13 @@ import City as city
 
 class HammurabiGame:
 
-    city = city.City()
-    city.start()
-
-
     def main(self):
         """
             Hauptmethode, welche beim Spielstart ausgeführt wird und bis Spielende durchläuft
 
         """
         city_instance = city.City()
+        city_instance.start()
 
 game = HammurabiGame()
 game.main()
