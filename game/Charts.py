69b28931ff04efe77a0e0e52921545bdd5fab20b
runisfun <franz.innerbichler@inode.at>
2019-05-15 23:42:29 +0200

    Neue Zeilen, Graph funktioniert, aber noch nicht interaktiv

--- game/Charts.py
+++ game/Charts.py
@@ -12,27 +12,45 @@ import json
 import pandas as pd
 import numpy as np
 
-
-
 LARGE_FONT=("Verdana",12)
 style.use("ggplot")
 
-f = Figure(figsize=(5, 5), dpi=100)
+
+f = Figure(figsize=(5,5), dpi=100)
 a = f.add_subplot(111)
+
+
+def animate():
+    pullData = open("sampleData.txt","r").read()
+    dataList = pullData.split('\n')
+    xList = []
+    yList = []
+    for eachLine in dataList:
+        if len(eachLine) > 1:
+            x,y = eachLine.split(',')
+            xList.append(int(x))
+            yList.append(int(y))
+
+    a.clear()
+    a.plot(xList, yList)
+
+
 
 class SeaofBTCapp(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        container = tk.Frame(self)
 
+        container = tk.Frame(self)
         container.pack(side="top", fill="both", expand="True")
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        frame = StartPage(container, self)
-        self.frames[StartPage] = frame
-        frame.grid(row=0, column=0, sticky="nsew")
+        for F in (StartPage, BCTe_Page):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
         self.show_frame(StartPage)
 
     def show_frame(self,cont):
@@ -45,51 +63,39 @@ def qf(stringtoprint):
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="StartPage")
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="ALpha bitcoin")
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Visit page1",
-                            command=lambda: qf("yoyo"))
+        button1 = ttk.Button(self, text="Visit page1", command=lambda: qf("yoyo"))
         button1.pack()
-
-app=SeaofBTCapp()
-app.mainloop()
-
+        button2 = ttk.Button(self, text="Visit page2", command=lambda: controller.show_frame(BCTe_Page))
+        button2.pack()
 
 
-def animate(i):
-    pullData = open("sampleData.txt","r").read()
-    dataList =pullData.split('\n')
-    xList=[]
-    yList=[]
-    for eachLine in dataList:
-        if len(eachLine) > 1:
-            x, y = eachLine.split(',')
-            xList.append(int(x))
-            yList.append(int(y))
-    a.clear()
-    a.plot(xList, yList)
 
-class Page(tk.Frame):
-    def __init__(selfself, parent, controller):
+class BCTe_Page(tk.Frame):
+    def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Graph Page")
         label.pack(pady=10, padx=10)
-        button1 = ttk.Button(self,text="Back to Home",
-                             command=lambda: controller.show_frame(StartPage))
+
+        button1 = ttk.Button(self,text="Back to Home", command=lambda: controller.show_frame(StartPage))
         button1.pack()
+
         f = Figure(figsize=(5, 5), dpi=100)
         a = f.add_subplot(111)
         a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
-        canvas =FigureCanvasTkAgg(f, self)
-        canvas.show()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand = True)
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
         toolbar = NavigationToolbar2Tk(canvas, self)
         toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-#root=Tk()
-#root.mainloop()
-# app= SeaofBTCapp()
-# ani = animation.FuncAnimate(f, animate, interval=1000)
+
+app= SeaofBTCapp()
+# ani = animation.FuncAnimation(f, animate(), interval=1000) #
+
+app.mainloop()
